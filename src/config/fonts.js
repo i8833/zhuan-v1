14fcd75fbcd8836a1ea5f7ae05da288ec4f939ec
i8833 <123848380+i8833@users.noreply.github.com>
2025-01-27 22:38:07 +0800

    Add files via upload

--- src/config/fonts.js
+++ src/config/fonts.js
@@ -2,7 +2,9 @@ export const localFonts = [
   { name: '方正小篆', path: '/fonts/fangzheng.ttf' },
   { name: '繁篆', path: '/fonts/fanzhuan.ttf' },
   { name: '金文大篆', path: '/fonts/dazhuan.ttf' },
- 
+  { name: '汉仪篆书', path: '/fonts/hyzs.ttf' },
+  { name: '九叠篆', path: '/fonts/jdz.ttf' }, 
+  { name: '甲骨文', path: '/fonts/jgw.ttf' },
  // { name: '方正小篆', path: '/fonts/fangzheng.ttf' },
  // { name: '繁篆', path: '/fonts/fanzhuan.ttf' },
  // { name: '金文大篆', path: '/fonts/dazhuan.ttf' },
