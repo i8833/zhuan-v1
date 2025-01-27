219da1db48f7372bdf0e7649df9182b95aadc037
i8833 <123848380+i8833@users.noreply.github.com>
2025-01-28 00:45:32 +0800

    Add files via upload

---
+++ src/config/fonts.js
@@ -0,0 +1,15 @@
+export const localFonts = [
+  { name: '方正小篆', path: '/fonts/fangzheng.ttf' },
+  { name: '繁篆', path: '/fonts/fanzhuan.ttf' },
+  { name: '金文大篆', path: '/fonts/dazhuan.ttf' },
+  { name: '汉仪篆书', path: '/fonts/hyzs.ttf' },
+  { name: '九叠篆', path: '/fonts/jdz.ttf' }, 
+  { name: '甲骨文', path: '/fonts/jgw.ttf' },
+  { name: '玉筋隶书', path: '/fonts/HYYuJinLi.ttf' },
+  { name: '华光繁隶书', path: '/fonts/hgfl.ttf' },
+
+ // { name: '方正小篆', path: '/fonts/fangzheng.ttf' },
+ // { name: '繁篆', path: '/fonts/fanzhuan.ttf' },
+ // { name: '金文大篆', path: '/fonts/dazhuan.ttf' },
+  // 在这里添加更多字体...
+]; 
