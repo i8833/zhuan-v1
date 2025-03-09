cfa4c7fa876d10823c6f898dd6b7484e0de6154a
i8833 <i88338833@gmail.com>
2025-03-09 17:26:13 +0800

    Update font configuration

---
+++ src/config/fonts.js
@@ -0,0 +1,26 @@
+export const localFonts = [
+  { name: '方正小篆', path: '/fonts/fangzheng.ttf' },
+  { name: '繁篆', path: '/fonts/fanzhuan.ttf' },
+  { name: '金文大篆', path: '/fonts/dazhuan.ttf' },
+  { name: '汉仪篆书', path: '/fonts/hyzs.ttf' },
+  { name: '九叠篆', path: '/fonts/jdz.ttf' }, 
+  { name: '行书-1', path: '/fonts/xing-1.ttf' },
+  { name: '草书-1', path: '/fonts/HengShanMaoBiCaoShu-2.ttf' },
+  { name: '草书-2', path: '/fonts/4089cao-01.ttf' },
+  { name: '隶书-1', path: '/fonts/LiBianLiShuTi-2.otf' },
+  { name: '隶书-2', path: '/fonts/li-02.ttf' },
+  { name: '隶书-3', path: '/fonts/4088li-01.ttf' },
+  { name: '隶书-4', path: '/fonts/4094li-03.ttf' },
+  { name: '隶书-5', path: '/fonts/4095li-04.ttf' }, 
+  { name: '行楷-1', path: '/fonts/xingkai-01.ttf' },
+  { name: '行楷-2', path: '/fonts/xingkai-02.ttc' },
+  { name: '甲骨文', path: '/fonts/jgw.ttf' },
+  
+
+
+
+
+
+
+  // 在这里添加更多字体...
+]; 
