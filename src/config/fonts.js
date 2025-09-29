bf5f6c4c16c79286d822e30a09ae14152f3b68dc
i8833 <123848380+i8833@users.noreply.github.com>
2025-09-29 09:53:05 +0800

    Update fonts.js

--- src/config/fonts.js
+++ src/config/fonts.js
@@ -1,28 +1,24 @@
 export const localFonts = [
+   
+  { name: '方正小篆', path: '/fonts/fangzheng.ttf' },
+  { name: '繁篆', path: '/fonts/fanzhuan.ttf' },
+  { name: '金文大篆', path: '/fonts/dazhuan.ttf' },
+  { name: '汉仪篆书', path: '/fonts/hyzs.ttf' },
+  { name: '九叠篆', path: '/fonts/jdz.ttf' }, 
+
   { name: '汉仪小楷', path: '/fonts/HanyiSentyFlorCalligraphy.ttf' },
   { name: '汉仪唐朝', path: '/fonts/HanyiSentyTang.ttf' },
   { name: '汉仪永乐', path: '/fonts/HanyiSentyYongleEncyclopedia.ttf' },
   { name: '汉仪赵体', path: '/fonts/HanyiSentyZhao.ttf' },
   { name: '新蒂君子兰', path: '/fonts/SentyOrchid.ttf' },
   { name: '汉仪文体', path: '/fonts/SentyWEN2017.ttf' },
-  
-  
-  { name: '方正小篆', path: '/fonts/fangzheng.ttf' },
-  { name: '繁篆', path: '/fonts/fanzhuan.ttf' },
-  { name: '金文大篆', path: '/fonts/dazhuan.ttf' },
-  { name: '汉仪篆书', path: '/fonts/hyzs.ttf' },
-  { name: '九叠篆', path: '/fonts/jdz.ttf' }, 
-  { name: '行书-1', path: '/fonts/xing-1.ttf' },
-  { name: '草书-1', path: '/fonts/HengShanMaoBiCaoShu-2.ttf' },
-  { name: '草书-2', path: '/fonts/4089cao-01.ttf' },
+
   { name: '隶书-1', path: '/fonts/LiBianLiShuTi-2.otf' },
   { name: '隶书-2', path: '/fonts/li-02.ttf' },
   { name: '隶书-3', path: '/fonts/4088li-01.ttf' },
   { name: '隶书-4', path: '/fonts/4094li-03.ttf' },
   { name: '隶书-5', path: '/fonts/4095li-04.ttf' }, 
-  { name: '行楷-1', path: '/fonts/xingkai-01.ttf' },
-  { name: '行楷-2', path: '/fonts/xingkai-02.ttc' },
-  { name: '甲骨文', path: '/fonts/jgw.ttf' },
+
   { name: '隶书-01', path: '/fonts/zxh.ttf' },
   { name: '隶书-02', path: '/fonts/DuanNingYingBiLiShu-2.ttf' },
   { name: '隶书-03', path: '/fonts/No.193-ShangShouXuanLiTi-2.ttf' },
@@ -34,8 +30,14 @@ export const localFonts = [
   { name: '隶书-09', path: '/fonts/HYYuJinLi-95F-2.ttf' },
   { name: '隶书-10', path: '/fonts/HYYuJinLi-85F-2.ttf' },
 
-  
-
+  // { name: '行书-1', path: '/fonts/xing-1.ttf' },
+  // { name: '草书-1', path: '/fonts/HengShanMaoBiCaoShu-2.ttf' },
+  // { name: '草书-2', path: '/fonts/4089cao-01.ttf' },
+  // { name: '行楷-1', path: '/fonts/xingkai-01.ttf' },
+  // { name: '行楷-2', path: '/fonts/xingkai-02.ttc' },
+  // { name: '甲骨文', path: '/fonts/jgw.ttf' },
+  //
+  //
 
   
 
