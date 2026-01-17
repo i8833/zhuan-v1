a527d40216c31b29e9d6fb653904ee3b51b00565
i8833 <123848380+i8833@users.noreply.github.com>
2026-01-17 15:52:43 +0800

    Add files via upload

---
+++ src/config/fonts.js
@@ -0,0 +1,51 @@
+export const localFonts = [
+   
+  { name: '方正小篆', path: '/fonts/fangzheng.woff2' },
+  { name: '繁篆', path: '/fonts/fanzhuan.woff2' },
+  { name: '金文大篆', path: '/fonts/dazhuan.woff2' },
+  { name: '汉仪篆书', path: '/fonts/hyzs.woff2' },
+  { name: '九叠篆', path: '/fonts/jdz.woff2' }, 
+
+  { name: '汉仪小楷', path: '/fonts/HanyiSentyFlorCalligraphy.woff2' },
+  { name: '汉仪唐朝', path: '/fonts/HanyiSentyTang.woff2' },
+  { name: '汉仪永乐', path: '/fonts/HanyiSentyYongleEncyclopedia.woff2' },
+  { name: '汉仪赵体', path: '/fonts/HanyiSentyZhao.woff2' },
+  { name: '新蒂君子兰', path: '/fonts/SentyOrchid.woff2' },
+  { name: '汉仪文体', path: '/fonts/SentyWEN2017.woff2' },
+
+  { name: '隶书-1', path: '/fonts/LiBianLiShuTi-2.woff2' },
+  { name: '隶书-2', path: '/fonts/li-02.woff2' },
+  { name: '隶书-3', path: '/fonts/4088li-01.woff2' },
+  { name: '隶书-4', path: '/fonts/4094li-03.woff2' },
+  { name: '隶书-5', path: '/fonts/4095li-04.woff2' }, 
+
+  { name: '隶书-01', path: '/fonts/zxh.woff2' },
+  { name: '隶书-02', path: '/fonts/DuanNingYingBiLiShu-2.woff2' },
+  { name: '隶书-03', path: '/fonts/No.193-ShangShouXuanLiTi-2.woff2' },
+  { name: '隶书-04', path: '/fonts/SanJiLiShuJianTi-Regular-2.woff2' },
+  { name: '隶书-05', path: '/fonts/JinXiQingLi-2.woff2' },
+  { name: '隶书-06', path: '/fonts/FGXiuQingTaiLiShu-2.woff2' },
+  { name: '隶书-07', path: '/fonts/QingNiaoHuaGuangFanLiBian-2.woff2' },
+  { name: '隶书-08', path: '/fonts/QingNiaoHuaGuangFanLiShu-2.woff2' },
+  { name: '隶书-09', path: '/fonts/HYYuJinLi-95F-2.woff2' },
+  { name: '隶书-10', path: '/fonts/HYYuJinLi-85F-2.woff2' },
+  { name: '甲骨文', path: '/fonts/jgw.woff2' },
+  
+  // { name: '行书-1', path: '/fonts/xing-1.woff2' },
+  // { name: '草书-1', path: '/fonts/HengShanMaoBiCaoShu-2.woff2' },
+  // { name: '草书-2', path: '/fonts/4089cao-01.woff2' },
+  // { name: '行楷-1', path: '/fonts/xingkai-01.woff2' },
+  // { name: '行楷-2', path: '/fonts/xingkai-02.ttc' },
+  // 
+  //
+  //
+
+  
+
+
+
+
+
+
+  // 在这里添加更多字体...
+]; 
